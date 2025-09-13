13f3f2918f1d4d715eee03d2746d7b21e0dfbabf
Bojan Dzogazovic <bdzogazovic@gmail.com>
2025-09-13 13:15:17 +0200

    navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import Navbar from "./components/Navbar";
+
 export default function HomePage() {
   return (
-    <div className="font-sans items-center justify-items-center">home</div>
+    <div className="font-sans items-center justify-items-center">
+      <Navbar />
+    </div>
   );
 }
