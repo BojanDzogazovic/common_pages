c2eeb8a876e296fd98a696f7fc9d80b2932559ad
Bojan Dzogazovic <bdzogazovic@gmail.com>
2025-09-15 14:10:05 +0200

    hero

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,15 @@
+export default function Hero() {
+  return (
+    <div
+      className="hero justify-center items-center text-center bg-sky-300"
+      style={{ width: "100%" }}
+    >
+      <p className="pt-40 pb-20 text-2xl font-bold text-sky-950">
+        Hero section
+      </p>
+      <button className="mb-25 bg-sky-950 px-5 py-3 text-white font-bold">
+        CTA
+      </button>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,11 @@
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 export default function HomePage() {
   return (
     <div className="font-sans items-center justify-items-center">
       <Navbar />
-      <div
-        className="hero justify-center items-center text-center bg-sky-300"
-        style={{ marginX: "100%" }}
-      >
-        <p className="pt-40 pb-20 text-2xl font-bold text-sky-950">
-          Hero section
-        </p>
-        <button className="mb-25 bg-sky-950 px-5 py-3 text-white font-bold">
-          CTA
-        </button>
-      </div>
+      <Hero />
     </div>
   );
 }
