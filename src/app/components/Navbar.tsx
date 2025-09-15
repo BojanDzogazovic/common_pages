541fcf7b7b5ac045a8f4ee3fd702804fdb775e35
Bojan Dzogazovic <bdzogazovic@gmail.com>
2025-09-15 21:05:06 +0200

    cards

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -53,7 +53,7 @@ export default function Navbar() {
           </div>
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex shrink-0 items-center">
-              <p>WeExist</p>
+              <p className="font-bold">WeExist</p>
             </div>
             <div
               className="hidden sm:ml-6 sm:block"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Testimonial from "./components/Testimonial";
+import Cards from "./components/Cards";
 
 export default function HomePage() {
   return (
     <div className="font-sans items-center justify-items-center">
       <Navbar />
       <Hero />
+      <Testimonial />
+      <Cards />
     </div>
   );
 }
